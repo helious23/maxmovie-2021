508ae932481baf5629bd2b1471a6aff7058e7081
Max16 <max16@naver.com>
2021-05-20 21:22:34 +0900

    #1.2 GET MOVIE Query

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,27 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      description_intro
+    }
+  }
+`;
 
 export default () => {
-  return <h1>Detail</h1>;
+  const { id } = useParams();
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id: parseInt(id) },
+  });
+  if (loading) {
+    return "Loading...";
+  }
+  if (data && data.movie) {
+    return data.movie.title;
+  }
 };
