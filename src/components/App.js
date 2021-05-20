a4d80935256728b3ad65dca5f363ec22e33bce01
Max16 <max16@naver.com>
2021-05-20 17:47:18 +0900

    #0.4 Router and Styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,18 @@
-import "./App.css";
+import { HashRouter as Router, Route } from "react-router-dom";
+import Detail from "../routes/Detail";
+import Home from "../routes/Home";
+import GlobalStyles from "./Globalstyles";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route path="/:id" component={Detail} />
+      </Router>
+      <GlobalStyles />
+    </>
+  );
 }
 
 export default App;
